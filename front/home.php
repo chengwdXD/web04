b101f62a1aa19233c2933e7de58253da20548f9f
weide <4a339047@stust.edu.tw>
2022-12-30 16:30:11 +0800

    完成最新消息區的more 和分頁

--- front/home.php
+++ front/home.php
@@ -1,24 +1,32 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 	<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-	<?php
-$ads=$Ad->all(['sh'=>1]);//找Ad資料表裡sh=1的資料
-foreach($ads as $ad){
-	echo $ad['text'];
-	echo "&nbsp;&nbsp;&nbsp;";
-}
+		<?php
+		$ads = $Ad->all(['sh' => 1]); //找Ad資料表裡sh=1的資料
+		foreach ($ads as $ad) {
+			echo $ad['text'];
+			echo "&nbsp;&nbsp;&nbsp;";
+		}
 
-// echo q("SElECT *,GROUP_CONCAT('---',`text`)as 'ad' FROM `ad` where sh=1 group by sh;")[0]['ad'];
-	?>
-</marquee>
+		// echo q("SElECT *,GROUP_CONCAT('---',`text`)as 'ad' FROM `ad` where sh=1 group by sh;")[0]['ad'];
+		?>
+	</marquee>
 	<div style="height:32px; display:block;"></div>
 	<!--正中央-->
+	
+	<div style="width:100%; padding:2px; height:290px;">
+		<div id="mwww" loop="true" style="width:100%; height:100%;">
+			<div style="width:99%; height:100%; position:relative;" class="cent">
+				沒有資料
+			</div>
+		</div>
+	</div>
 	<script>
 		var lin = new Array();
 		<?php
-$mvs=$Mvim->all(['sh'=>1]);
-foreach($mvs as $mv){
-	echo "lin.push('./upload/{$mv['img']}');";
-}
+		$mvs = $Mvim->all(['sh' => 1]);
+		foreach ($mvs as $mv) {
+			echo "lin.push('./upload/{$mv['img']}');";
+		}
 
 		?>
 		var now = 0;
@@ -36,17 +44,31 @@ foreach($mvs as $mv){
 				now = 0;
 		}
 	</script>
-	<div style="width:100%; padding:2px; height:290px;">
-		<div id="mwww" loop="true" style="width:100%; height:100%;">
-			<div style="width:99%; height:100%; position:relative;" class="cent">
-			沒有資料
-		</div>
-		</div>
-	</div>
 	<div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
 		<span class="t botli">最新消息區
+			<?php
+			if ($News->count(['sh' => 1]) > 5) {
+				echo "<a href='?do=news' style='float:right'>";
+				echo "More...";
+				echo "</a>";
+			}
+
+			?>
 		</span>
 		<ul class="ssaa" style="list-style-type:decimal;">
+			<?php
+			$news = $News->all(['sh' => 1], "limit 5");
+			foreach ($news as $n) {
+				echo "<li>";
+				echo mb_substr($n['text'], 0, 25) . "...";
+				echo "<span class='all' style='display:none'>";
+				echo $n['text'];
+				echo "</span>";
+				echo "</li>";
+			}
+
+
+			?>
 		</ul>
 		<div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 		<script>
